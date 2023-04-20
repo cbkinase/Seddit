03d305c4e809da475efd40c85f2a4eafc644cc46
cbkinase <cambeck366@gmail.com>
2023-04-20 02:21:11 -0400

    changes to signup/login

--- react-app/src/components/LoginFormModal/index.js
+++ react-app/src/components/LoginFormModal/index.js
@@ -1,57 +1,113 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
+import { useHistory } from "react-router-dom";
 import "./LoginForm.css";
 
 function LoginFormModal() {
-  const dispatch = useDispatch();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
-  const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const [credential, setCredential] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState({});
+    const { closeModal } = useModal();
+    const [validationErrors, setValidationErrors] = useState({});
+    const [submitDisabled, setSubmitDisabled] = useState(true);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = await dispatch(login(email, password));
-    if (data) {
-      setErrors(data);
-    } else {
-        closeModal()
-    }
-  };
+    useEffect(() => {
+        const errors = {};
+        if (credential.length < 4) {
+            errors.credential =
+                "Email must be at least 4 characters";
+        }
+        if (password.length < 6) {
+            errors.password = "Password must be at least 6 characters";
+        }
 
-  return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-    </>
-  );
+        setValidationErrors(errors);
+
+        if (Object.keys(errors).length === 0) {
+            setSubmitDisabled(false);
+        } else setSubmitDisabled(true);
+    }, [credential, password]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErrors([]);
+        const data = await dispatch(login(credential, password ))
+        if (data) setErrors(data)
+        else closeModal()
+    };
+
+    const handleDemoLogin = async (e) => {
+        e.preventDefault();
+        await dispatch(login("demo@aa.io", "password"))
+            .then(closeModal)
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(Object.values(data.errors));
+            });
+    };
+
+    return (
+        <div className="login-container">
+            <h1 id="title">Log In</h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+                <ul>
+                    {Object.values(errors).map((error, idx) => (
+                        <li className="errors" key={idx}>
+                            {error}
+                        </li>
+                    ))}
+                </ul>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            type="text"
+                            value={credential}
+                            placeholder="Email"
+                            onChange={(e) => setCredential(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {/* {validationErrors.credential && (
+                        <p className="errors">*{validationErrors.credential}</p>
+                    )} */}
+                </div>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {/* {validationErrors.password && (
+                        <p className="errors">*{validationErrors.password}</p>
+                    )} */}
+                </div>
+                <button
+                    className="decorated-button button-needs-adjustment alt-color-button"
+                    disabled={submitDisabled}
+                    id="submit"
+                    type="submit"
+                >
+                    Log In
+                </button>
+                <button
+                    className="decorated-button button-needs-adjustment"
+                    onClick={handleDemoLogin}
+                    id="demo"
+                >
+                    Demo User
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default LoginFormModal;
--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -1,83 +1,131 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { signUp } from "../../store/session";
 import "./SignupForm.css";
 
 function SignupFormModal() {
-	const dispatch = useDispatch();
-	const [email, setEmail] = useState("");
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const [confirmPassword, setConfirmPassword] = useState("");
-	const [errors, setErrors] = useState([]);
-	const { closeModal } = useModal();
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState({});
+    const { closeModal } = useModal();
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [submitDisabled, setSubmitDisabled] = useState(true);
+    const [validationErrors, setValidationErrors] = useState({});
+    const signUpDependencies = [
+        email,
+        username,
+        password,
+        confirmPassword,
+    ];
+    useEffect(() => {
+        const errors = {};
+        if (signUpDependencies.some((formInput) => formInput.length === 0))
+            errors.empty = "Nonzero input length required";
+        if (username.length < 4)
+            errors.username = "Username must be at least 4 characters";
+        if (password.length < 6)
+            errors.password = "Password must be at least 6 characters";
+        if (password !== confirmPassword)
+            errors.confirmation = "Passwords do not match";
+        setValidationErrors(errors);
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, email, password));
-			if (data) {
-				setErrors(data);
-			} else {
-				closeModal();
-			}
-		} else {
-			setErrors([
-				"Confirm Password field must be the same as the Password field",
-			]);
-		}
-	};
+        if (Object.keys(errors).length === 0) setSubmitDisabled(false);
+        else setSubmitDisabled(true);
+    }, signUpDependencies);
 
-	return (
-		<>
-			<h1>Sign Up</h1>
-			<form onSubmit={handleSubmit}>
-				<ul>
-					{errors.map((error, idx) => (
-						<li key={idx}>{error}</li>
-					))}
-				</ul>
-				<label>
-					Email
-					<input
-						type="text"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Username
-					<input
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Password
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Confirm Password
-					<input
-						type="password"
-						value={confirmPassword}
-						onChange={(e) => setConfirmPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<button type="submit">Sign Up</button>
-			</form>
-		</>
-	);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (password === confirmPassword) {
+            setErrors([]);
+            return dispatch(signUp(username, email, password))
+                .then(closeModal)
+                .catch(async (res) => {
+                    setHasSubmitted(true);
+                    const data = await res.json();
+                    if (data && data.errors) setErrors(data.errors);
+                });
+        }
+        return setErrors([
+            "Confirm Password field must be the same as the Password field",
+        ]);
+    };
+
+    return (
+        <div className="login-container">
+            <h1 id="title">Sign Up</h1>
+            <form className="signup-form" onSubmit={handleSubmit}>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            placeholder="Email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {hasSubmitted && errors.email && (
+                        <p className="errors">*{errors.email}</p>
+                    )}
+                </div>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            placeholder="Username - 4 characters minimum"
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {hasSubmitted && errors.username && (
+                        <p className="errors">*{errors.username}</p>
+                    )}
+                </div>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            placeholder="Password - 6 characters minimum"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                    {hasSubmitted && errors.password && (
+                        <p className="errors">*{errors.password}</p>
+                    )}
+                </div>
+                <div className="form-item">
+                    <label>
+                        <input
+                            className="input-field"
+                            placeholder="Confirm Password"
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                        />
+                    </label>
+                </div>
+                <button
+                    className="decorated-button alt-color-button"
+                    disabled={submitDisabled}
+                    id="submit"
+                    type="submit"
+                >
+                    Sign Up
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default SignupFormModal;
