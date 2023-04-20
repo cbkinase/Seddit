e6d9c2e14f3d065eb5bae5125787269e5d987c65
cbkinase <cambeck366@gmail.com>
2023-04-19 21:51:22 -0400

    mostly done with subreddit routes

--- app/api/subreddit_routes.py
+++ app/api/subreddit_routes.py
@@ -10,7 +10,6 @@ def get_all_subreddits():
     """
     Returns all subreddits, regardless of user login status.
     """
-
     all_subreddits = Subreddit.query.all()
     return {"Subreddits": {subreddit.id : subreddit.to_dict() for subreddit in all_subreddits}}
 
@@ -31,6 +30,10 @@ def get_subreddit_by_pk(subreddit_id):
     Return the information association with a particular subreddit by primary key.
     """
     subreddit = Subreddit.query.get(subreddit_id)
+
+    if not subreddit:
+        return {"errors": ["Subreddit not found"]}
+
     return subreddit.to_dict()
 
 
@@ -40,6 +43,10 @@ def get_subreddit_by_name(subreddit_name):
     Return the information association with a particular subreddit by name.
     """
     subreddit = Subreddit.query.filter(Subreddit.name == subreddit_name).first()
+
+    if not subreddit:
+        return {"errors": ["Subreddit not found"]}
+
     return subreddit.to_dict()
 
 
@@ -54,3 +61,43 @@ def create_subreddit():
     db.session.add(new_subreddit)
     db.session.commit()
     return new_subreddit.to_dict()
+
+
+
+@subreddit_routes.route("/<int:subreddit_id>", methods=["PUT"])
+@login_required
+def edit_subreddit(subreddit_id):
+    """
+    Edit an existing subreddit, allowable if you are the Owner.
+    """
+    subreddit = Subreddit.query.get(subreddit_id)
+
+    if not subreddit:
+        return {"errors": ["Subreddit not found"]}
+
+    if current_user.id != subreddit.owner_id:
+        return {"errors": ["Attempted editor is not owner"]}, 403
+
+    for key, value in request.get_json().items():
+        setattr(subreddit, key, value)
+
+    db.session.commit()
+    return subreddit.to_dict()
+
+
+@subreddit_routes.route("/<int:subreddit_id>", methods=["DELETE"])
+@login_required
+def delete_subreddit(subreddit_id):
+    subreddit = Subreddit.query.get(subreddit_id)
+
+    if not subreddit:
+        return {"errors": ["Subreddit not found"]}
+
+    if current_user.id != subreddit.owner_id:
+        return {"errors": ["Attempted deleter is not owner"]}, 403
+
+    db.session.delete(subreddit)
+    db.session.commit()
+    return {
+        "success": "Successfully deleted"
+     }
