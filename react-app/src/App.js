4800dd0bf2faa55997ada605778b2d88ff24de9e
cbkinase <cambeck366@gmail.com>
2023-04-21 15:10:04 -0400

    added logout button on user self page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -19,31 +19,31 @@ function App() {
     const subreddits = useSelector((state) => state.subreddits.Subreddits);
     const user = useSelector((state) => state.session.user);
 
-    const shortenWord = (word, long=20) => {
+    const shortenWord = (word, long = 20) => {
         if (!word) return null;
         if (word.length < long) return word;
-        return word.slice(0,long) + "..."
-    }
+        return word.slice(0, long) + "...";
+    };
 
-    const ellipsisIfLong = (paragraph, long=20) => {
+    const ellipsisIfLong = (paragraph, long = 20) => {
         if (!paragraph) return null;
         let wordArr = paragraph.split(" ");
         let newStr = "";
         if (wordArr.length > long) {
             for (let i = 0; i < long; i++) {
                 newStr += wordArr[i];
-                if (i !== long - 1) newStr += " "
+                if (i !== long - 1) newStr += " ";
             }
 
-                newStr += "...";
+            newStr += "...";
             return newStr;
         }
         return paragraph;
     };
 
     const capitalizeFirstLetter = (word) => {
-        return word[0].toUpperCase() + word.slice(1)
-    }
+        return word[0].toUpperCase() + word.slice(1);
+    };
 
     const handleJoinCommunity = async (subredditId, userId, subredditName) => {
         const res = await fetch(`/api/s/${subredditId}/subscribers`, {
@@ -55,7 +55,8 @@ function App() {
             let btn = document.getElementById(
                 `subreddit-${subredditId}-button`
             );
-            btn.classList = "act-subreddit-btn button-leave remove-bottom-margin";
+            btn.classList =
+                "act-subreddit-btn button-leave remove-bottom-margin";
             btn.innerText = "Joined";
             btn.onclick = (e) => alert(`Already joined r/${subredditName}!`);
             // btn.onclick = (e) => handleLeaveCommunity(subredditId, userId);
@@ -75,7 +76,8 @@ function App() {
             let btn = document.getElementById(
                 `subreddit-${subredditId}-button`
             );
-            btn.classList = "act-subreddit-btn button-join remove-bottom-margin";
+            btn.classList =
+                "act-subreddit-btn button-join remove-bottom-margin";
             btn.innerText = "Left Community";
             btn.onclick = (e) => alert(`Already left r/${subredditName}!`);
             // btn.onclick = (e) => handleJoinCommunity(subredditId, userId);
@@ -94,26 +96,37 @@ function App() {
                             {Object.values(subreddits).map((subreddit) => (
                                 <div className="box-dec-1 subreddit-short-container">
                                     <span className="subreddit-abridged-top">
-
                                         <span className="subreddit-title-preview">
-                                        <NavLink className="subreddit-title-nav" to={`/r/${subreddit.name}`}>
-                                            <img
-                                                className="subreddit-preview-img"
-                                                src={subreddit.main_pic}
-                                            ></img>
+                                            <NavLink
+                                                className="subreddit-title-nav"
+                                                to={`/r/${subreddit.name}`}
+                                            >
+                                                <img
+                                                    className="subreddit-preview-img"
+                                                    src={subreddit.main_pic}
+                                                ></img>
                                             </NavLink>
-                                            <NavLink className="subreddit-title-nav" to={`/r/${subreddit.name}`}>
-                                            <h1 className="card-title">
-                                                r/{subreddit.name}{" "}
-                                            </h1></NavLink>
+                                            <NavLink
+                                                className="subreddit-title-nav"
+                                                to={`/r/${subreddit.name}`}
+                                            >
+                                                <h1 className="card-title">
+                                                    r/{subreddit.name}{" "}
+                                                </h1>
+                                            </NavLink>
                                             <span className="subreddit-preview-creator">
                                                 {" "}
                                                 • created by{" "}
-                                                <NavLink className="card-username-link" to={`/u/${subreddit.owner_info.username}`}>
-                                                {
-                                                    shortenWord(subreddit.owner_info
-                                                        .username, 10)
-                                                }</NavLink>{" "}
+                                                <NavLink
+                                                    className="card-username-link"
+                                                    to={`/u/${subreddit.owner_info.username}`}
+                                                >
+                                                    {shortenWord(
+                                                        subreddit.owner_info
+                                                            .username,
+                                                        10
+                                                    )}
+                                                </NavLink>{" "}
                                                 {moment(
                                                     Date.parse(
                                                         subreddit.created_at
@@ -160,12 +173,19 @@ function App() {
                                                 Already Joined
                                             </button>
                                         )}
-                                    <NavLink className="subreddit-title-nav" to={`/r/${subreddit.name}`}>
-                                    <h2 className="card-info">
-                                        {ellipsisIfLong(subreddit.about)}
-                                    </h2></NavLink>
+                                    <NavLink
+                                        className="subreddit-title-nav"
+                                        to={`/r/${subreddit.name}`}
+                                    >
+                                        <h2 className="card-info">
+                                            {ellipsisIfLong(subreddit.about)}
+                                        </h2>
+                                    </NavLink>
                                     <h3 className="card-category">
-                                        {subreddit.category && capitalizeFirstLetter(subreddit.category)}
+                                        {subreddit.category &&
+                                            capitalizeFirstLetter(
+                                                subreddit.category
+                                            )}
                                     </h3>
                                 </div>
                             ))}
