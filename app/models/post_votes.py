76f054f2fd6b937aa586e33b50052efcd425cf9d
cbkinase <cambeck366@gmail.com>
2023-05-23 18:48:32 -0400

    votes tables

---
+++ app/models/post_votes.py
@@ -0,0 +1,32 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from random import choice
+
+class PostVote(db.Model):
+    __tablename__ = 'post_votes'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
+    post_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("posts.id")))
+    vote = db.Column(db.String, nullable=False)
+
+    user = db.relationship("User", back_populates="post_votes")
+    post = db.relationship("Post", back_populates="votes")
+
+    @classmethod
+    def create(cls, qty, users, posts):
+        vote_options = ["upvote", "downvote", "upvote", "upvote"] # seed more upvotes than downvotes
+        return [cls(
+            user=choice(users),
+            post=choice(posts),
+            vote=choice(vote_options) ) for _ in range(qty)]
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'post_id': self.post_id,
+            'vote': self.vote,
+        }
---
+++ app/seeds/comment_votes.py
@@ -0,0 +1,27 @@
+from app.models import db, environment, SCHEMA, CommentVote
+from sqlalchemy.sql import text
+
+
+def seed_comment_votes(users, comments):
+    dummy_votes = CommentVote.create(50000, users, comments)
+
+    for vote in dummy_votes:
+        db.session.add(vote)
+        db.session.commit()
+
+    return dummy_votes
+
+
+# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
+# have a built in function to do this. With postgres in production TRUNCATE
+# removes all the data from the table, and RESET IDENTITY resets the auto
+# incrementing primary key, CASCADE deletes any dependent entities.  With
+# sqlite3 in development you need to instead use DELETE to remove all data and
+# it will reset the primary keys for you as well.
+def undo_comment_votes():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.comment_votes RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM comment_votes"))
+
+    db.session.commit()
