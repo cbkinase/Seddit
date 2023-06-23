f73c21ef6504584ebe03db838620c103a9ca8172
cbkinase <cambeck366@gmail.com>
2023-06-23 04:33:49 -0400

    search backend started

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ from .api.auth_routes import auth_routes
 from .api.subreddit_routes import subreddit_routes
 from .api.post_routes import post_routes
 from .api.comment_routes import comment_routes
+from .api.search_routes import search_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -34,6 +35,7 @@ app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(subreddit_routes, url_prefix="/api/s")
 app.register_blueprint(post_routes, url_prefix="/api/posts")
 app.register_blueprint(comment_routes, url_prefix="/api/comments")
+app.register_blueprint(search_routes, url_prefix="/api/search")
 db.init_app(app)
 Migrate(app, db)
 
