f9903371ca667a013b4fbbb06b8ed73564215b18
cbkinase <cambeck366@gmail.com>
2023-06-23 20:47:52 -0400

    change to seeding... again

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -7,7 +7,7 @@ def seed_comments(users, posts, qty=1500):
     dummy_comments = Comment.create(qty, users, posts)
 
     for chnk in chunk(dummy_comments):
-        db.session.add_all(chnk)
+        db.session.bulk_save_objects(chnk)
         db.session.commit()
 
     return dummy_comments
