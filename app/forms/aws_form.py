f844cff219c1aa8272d111a7f8e41451e174fc44
cbkinase <cambeck366@gmail.com>
2023-07-07 23:44:15 -0400

    AWS form generation more generic now

--- app/forms/aws_form.py
+++ app/forms/aws_form.py
@@ -3,13 +3,19 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SubmitField
 from ..aws_helpers import ALLOWED_EXTENSIONS
 
+
 def create_upload_form(field_name):
     """Factory for creating UploadForms with needed attrs"""
+    allowable_field_types = ["attachment", "avatar"]
+
+    if field_name not in allowable_field_types:
+        raise ValueError(f"Invalid field name '{field_name}'. Valid options are: {', '.join(allowable_field_types)}.")
+
     class UploadForm(FlaskForm):
-        if field_name == "attachment":
-            attachment = FileField("attachment", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
-        if field_name == "avatar":
-            avatar = FileField("avatar", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+        validators = [FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))]
+        temp_field = FileField(field_name, validators=validators)
         submit = SubmitField("Create")
 
+    setattr(UploadForm, field_name, UploadForm.temp_field)
+    delattr(UploadForm, 'temp_field')
     return UploadForm()
